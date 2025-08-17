c57d9e80764615f2cd2d6ce9d5e1dbdf5470fcc3
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-08-17 23:28:26 +0100

    Update crossover.h

--- EC/crossover.h
+++ EC/crossover.h
@@ -16,28 +16,29 @@
 #include <string.h>
 #include <time.h>
 
-#define     EC_POP_SIZE         200
+#define     EC_POP_SIZE         50
 #define     EC_STRING           "HARRY CLARK"
 #define     EC_STRLEN           strlen(EC_STRING)
-#define     EC_MAX_GEN          100
-#define     EC_MUT_RATE         15
-#define     EC_CRS_RATE         85
+#define     EC_MAX_GEN          200
+#define     EC_MUT_RATE         3
+#define     EC_CRS_RATE         70
 #define     EC_SELECT           5
-#define     EC_PFRM             10
-#define     EC_GUIDE_PROB       30
+#define     EC_PFRM             5
+#define     EC_GUIDE_PROB       20
 #define     EC_CS               46
 
 #define EC_STATUS() \
-    printf("TARGET STRING: %s\n", EC_STRING);                   \
-    printf("CURRENT CROSSOVER TYPE: %s\n", CROSSOVER_TYPE_STRINGS[CURRENT_CROSSOVER_METHOD]); \
-    printf("POPULATION SIZE: %d\n", EC_POP_SIZE);               \
-    printf("MAXIMUM GENERATIONS: %d\n", EC_MAX_GEN);            \
-    printf("MUTATION RATE: %d%%\n", EC_MUT_RATE);               \
-    printf("CROSSOVER RATE: %d%%\n", EC_CRS_RATE);              \
-    printf("CHROMOSOMES: %d\n",     EC_CS);                     \
-    printf("TOURNAMENT SIZE: %d\n", EC_SELECT);                 \
-    printf("ELITE PRESERVATION: %d\n", EC_PFRM);                \
-    printf("GUIDED PROBABILITY RATE: %d\n\n", EC_GUIDE_PROB);   \
+    printf("EVO CONFIG:\n"); \
+    printf("    TARGET STRING: %s\n", EC_STRING);                   \
+    printf("    CURRENT CROSSOVER TYPE: %s\n", CROSSOVER_TYPE_STRINGS[CURRENT_CROSSOVER_METHOD]); \
+    printf("    POPULATION SIZE: %d\n", EC_POP_SIZE);               \
+    printf("    MAXIMUM GENERATIONS: %d\n", EC_MAX_GEN);            \
+    printf("    MUTATION RATE: %d%%\n", EC_MUT_RATE);               \
+    printf("    CROSSOVER RATE: %d%%\n", EC_CRS_RATE);              \
+    printf("    CHROMOSOMES: %d\n",     EC_CS);                     \
+    printf("    TOURNAMENT SIZE: %d\n", EC_SELECT);                 \
+    printf("    ELITE PRESERVATION: %d\n", EC_PFRM);                \
+    printf("    GUIDED PROBABILITY RATE: %d\n\n", EC_GUIDE_PROB);   \
     printf("\n")                                                \
 
 // CROSSOVER METHODS
@@ -76,6 +77,11 @@ int SELECTION(PERSON* P);
 int COMPARE_PERSON(const void* PERSON_A, const void* PERSON_B);
 
 void POPULATION_INIT(PERSON* POPULATION);
+void POPULOUS_FITNESS(PERSON* POP, int SIZE);
+void PRESERVE_ELITE(PERSON* CURRENT, PERSON* NEW_POP, int ELITE);
+
+void GEN_OFFSPRING(PERSON* CURRENT, PERSON* NEW_POP, int START, int END);
+void COPY_POP(PERSON* SRC, PERSON* DEST, int SIZE);
 
 static TYPE CURRENT_CROSSOVER_METHOD;
 static const char* CROSSOVER_TYPE_STRINGS[] = 
