43b7599f1f6fbc9495bf0c4aff26ba1fa28d6fa9
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-08-17 19:33:38 +0100

    modularised into header

--- EC/crossover.c
+++ EC/crossover.c
@@ -5,18 +5,6 @@
 // ALLOWS FOR THE GRADUAL MUTATION/REPRODUCTION OF AN ALGORITHM OVER THE COURSE
 // OF IT'S RUNTIME
 
-// SYSTEM INCLUDES
+// NESTED INCLUDES
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-#define     EC_POP_SIZE         200
-#define     EC_STRING           "HARRY CLARK"
-#define     EC_STRLEN           strlen(EC_STRING)
-#define     EC_MAX_GEN          100
-#define     EC_MUT_RATE         15
-#define     EC_CRS_RATE         85
-#define     EC_SELECT           5
-#define     EC_PFRM             10
+#include "crossover.h"
