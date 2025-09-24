8827d724615cf9f13f0bf4e02cac68e50b7e3fe6
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-09-24 19:36:35 +0100

    initial TSP solution for Week 1 lab

---
+++ TSP/tsp.h
@@ -0,0 +1,20 @@
+// COPYRIGHT (C) HARRY CLARK 2025
+// CS3_CI TRAVELLING SALESMAN PROBLEM 
+
+// THIS FILE PERTAINS TOWARDS THE MODULARISATION OF AREAS ENCOMPASSING THE TSP
+// AS PER MODULE AND LAB CONTENTS OF CS3_CI
+
+#ifndef TSP_H
+#define TSP_H
+
+// SYSTEM INCLUDES
+
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#define     TSP_MAX_CITIES      5
+#define     TSP_INF             INT_MAX             
+
+#endif
