3bd8a7db5212963bdf336c22260a82c737ff8023
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-09-24 19:48:25 +0100

    added extended representations

--- TSP/tsp.h
+++ TSP/tsp.h
@@ -14,7 +14,68 @@
 #include <stdio.h>
 #include <string.h>
 
-#define     TSP_MAX_CITIES      5
-#define     TSP_INF             INT_MAX             
+#if defined(USE_TSP)
+    #define USE_TSP
+#else
+    #define USE_TSP
 
+    #define     TSP_MAX_CITIES      5
+    #define     TSP_INF             INT_MAX
+
+    // DEFINE AN ENUM WHICH REPRESENTS THE VARIOUS ALGORITHM TYPES
+    // THAT CAN BE TESTED AGAINST
+    typedef enum
+    {
+        TSP_BRUTE_FORCE,
+        TSP_NEAREST,
+        TSP_HELD_KARP
+
+    } TSP_ALGO;
+
+    // DEFINE THE BASELINE IMPLEMENTATION FOR HOW
+    // WE CAN REPRESENT A CITY
+    typedef struct
+    {
+        char NAME[32];          
+        int X;                  
+        int Y;
+
+    } TSP_CITY;
+
+    // DEFINE THE BASELINE IMPLEMENTAITON FOR HOW
+    // WE CAN REPRESENT THE MATRICES BETWEEN CITY
+
+    // THIS IS THE MEANS BY WHICH WE CAN DETERMINE THE
+    // DISTANCE BETWEEN CITIES
+    typedef struct
+    {
+        int TSP_MATRIX[TSP_MAX_CITIES][TSP_MAX_CITIES];         // 2D ARRAY FOR MAX DIST BETWEEN
+        int TSP_DIST_SIZE;
+            
+    } TSP_DIST;
+
+    // DEFINE THE BASELINE IMPLEMENTATION FOR HOW
+    // WE CAN REPRESENT THE PATHING THAT STARTS AT
+    // A SPECIFIC CITY (TOUR)
+    typedef struct
+    {
+        int TSP_PATH[TSP_MAX_CITIES + 1];
+        int TSP_TOTAL_DIST;
+        int TSP_CITY_COUNT;
+        TSP_ALGO ALGO;
+
+    } TSP_TOUR;
+
+    // ALL ENCOMPASSING TSP STRUCT TO ACCESS THESE METHODS
+    typedef struct
+    {
+        TSP_CITY CITY[TSP_MAX_CITIES];
+        TSP_DIST DIST;
+        TSP_TOUR TOUR;
+        int CITY_COUNT;
+
+    } TSP_STATE;
+
+
+#endif
 #endif
