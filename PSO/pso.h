3ceb1bbe82bacfdf3744bc3c77121b6eff355bef
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-10-03 21:31:49 +0100

    added remaining base params

--- PSO/pso.h
+++ PSO/pso.h
@@ -53,7 +53,86 @@
             PSO_VALLEY,
 
         } PSO_FITNESS_TYPE;
- 
+
+        // DEFINE THE BASIS BY WHICH THE PSO
+        // WILL BE DEFINED, BASED ON IT'S RESPECTIVE PARAMS
+        typedef struct
+        {
+            double POSITION[PSO_MAX_DIMENSIONS];
+            double VELOCITY[PSO_MAX_DIMENSIONS];
+            double BEST_POS[PSO_MAX_DIMENSIONS];
+            double BEST_FITNESS;
+            double CURRENT_FITNESS;
+            int STAGNATE;
+            
+        } PSO_PARTICLE;
+
+        // DEFINE THE BASIS FOR THE SWARM ITSELF
+        // ACCESSES ALL OF THE CONCURRENT PARTICLES
+        // AND THEIR "BEST FITNESS" PARAMS
+        typedef struct
+        {
+            PSO_PARTICLE PARTICLE[PSO_MAX_PARTICLES];
+            double GLOBAL_POS;
+            double GLOBAL_FITNESS;
+            
+            union
+            {
+                int COUNT;
+                int ITERATION;
+                int CONVERGENCE;
+                
+            } PARTICLE;
+
+        }  PSO_SWARM;
+
+        // DEFINE ALL OF THE PARAMETERS
+        // SPECIFIC TO THE GP ITSELF
+        // HELPS WITH OPTMISING THE SPACE AS WE CAN REDUCE
+        // THE AMOUNT OF OVERHEAD ENCOMPASSED BY SEARCH SPACE
+        typedef union
+        {   
+            double MUTATION;
+            double CROSSOVER;
+            double ELITISM;
+            double TOUR_SIZE;
+            double MAX_DEPTH;
+            double POP_SIZE;
+
+        } PSO_GP;
+
+        typedef struct
+        {
+            PSO_SWARM SWARM;
+            int BOUNDS;
+            PSO_FITNESS_TYPE FITNESS;
+            PSO_GP GP;
+            int DIMENSIONS;
+            double CONV_THRESHOLD;
+
+        } PSO;
+
+        typedef enum
+        {
+            PSO_ERROR_NONE = 0,
+            PSO_ERROR_OOB,
+            PSO_ERROR_PARTICLE,
+            PSO_ERROR_CONVERGENCE,
+            PSO_ERROR_FITNESS,
+            PSO_ERROR_DIM
+
+        } PSO_ERROR;
+
+        typedef enum
+        {
+            NONE = 'N',
+            OOB = 'O',
+            PART = 'P',
+            CONV = 'C',
+            FIT = 'F',
+            DIM = 'D'
+
+        } PSO_ERROR_OP;
 
 #endif
 #endif
